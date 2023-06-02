078194850ae8c20b57fd6a9def37e699364c8fda
lalobarajasp <ing.barajas.perez@gmail.com>
2023-06-02 16:20:29 -0600

    updated

--- src/main/java/com/example/onlineLearning/user/email/EmailService.java
+++ src/main/java/com/example/onlineLearning/user/email/EmailService.java
@@ -27,7 +27,7 @@ public class EmailService implements EmailSender {
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your email");
-            helper.setFrom("hello@amigoscode.com");
+            helper.setFrom("online_learning@example.com");
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
